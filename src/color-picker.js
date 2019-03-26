8751e47df9e78732bd1465fdf4c059618a88b86d
skatz <shai@webiks.com>
2019-03-26 17:13:02 +0200

    fixup! first commit

--- src/color-picker.js
+++ src/color-picker.js
@@ -1,11 +1,17 @@
 class ColorPicker extends HTMLElement {
-    constructor(){
+    constructor() {
         super()
-        this.attachShadow({mode:'open'})
+        this.attachShadow({ mode: 'open' })
         var wrapper = document.createElement('span')
-        wrapper.setAttribute('class','wrapper')
+        wrapper.setAttribute('class', 'wrapper')
         wrapper.innerText = 'yay succeed'
     }
 }
 
-customElements.define('colorPicker',ColorPicker)
+customElements.define('color-picker', ColorPicker)
+
+window.onload = () => {
+    const colotPicker = document.createElement('color-picker')
+    document.body.appendChild(colotPicker)
+}
+
