9c32f85b31687f09f274690c68161c45c0b041a8
skatz <shai@webiks.com>
2019-05-15 09:50:40 +0300

    modify index.html/ index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const colorPicker = document.createElement('color-picker')
 colorPicker.addEventListener('click',()=>{
     colorPicker.close()
 })
-document.body.appendChild(colorPicker)
-const button = document.createElement('button')
-button.innerText = 'open color picker'
-button.addEventListener('click',()=>{
-    colorPicker.open()
-})
-document.body.appendChild(button)
+window.onload = ()=>{
+    document.body.appendChild(colorPicker)
+    const button = document.createElement('button')
+    button.innerText = 'open color picker'
+    button.addEventListener('click',()=>{
+        colorPicker.open()
+    })
+    document.body.appendChild(button)
+
+}
